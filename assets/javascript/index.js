3868ea52a41ac5f447991e9c2666e372fc33045b
Jonatas <jonatasvsales@gmail.com>
2015-06-19 17:02:37 -0300

    Getting rid of jQuery

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,26 +1,24 @@
-$(document).ready(function () {
-    $(document).scroll(function () {
-        var scrollTop = $(document).scrollTop();
-        if (scrollTop > 2000) {
-            circles_action();
-            $(document).unbind("scroll");
-        }
-    });
-
-    function circles_action() {
-        for (var i = 1; i <= 8; i++) {
-            var child = document.getElementById('circles-' + i);
-            var percentage = $("#circles-" + i).attr("percent");
-            Circles.create({
-                id: child.id,
-                percentage: percentage,
-                radius: 80,
-                width: 15,
-                number: percentage,
-                text: '%',
-                colors: ['#fff', '#2980b9']
-            });
-        }
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.onscroll = function() {
+    if (document.body.scrollTop > 2000) {
+      circles_action();
+      document.onscroll = false;
     }
+  };
 
+  function circles_action() {
+    for (var i = 1; i <= 8; i++) {
+      var child = document.getElementById('circles-' + i);
+      var percentage = document.getElementById("circles-" + i).getAttribute("percent");;
+      Circles.create({
+        id: child.id,
+        percentage: percentage,
+        radius: 80,
+        width: 15,
+        number: percentage,
+        text: '%',
+        colors: ['#fff', '#2980b9']
+      });
+    }
+  }
 });
