79e61ff1071bca02da0f774313e9e5b9dabee0b9
Jonatas Sales <jonatasvsales@gmail.com>
2016-06-07 17:02:50 -0700

    New website structure

---
+++ assets/js/background.js
@@ -0,0 +1,14 @@
+var setBackground = function() {
+  var t = new Trianglify({
+    noiseIntensity: 0,
+    cellsize: 50,
+    fillOpacity: 0.5
+  });
+  var pattern;
+  var width = window.innerWidth;
+  var height = document.body.parentNode.offsetHeight;
+  pattern = t.generate(width, height);
+  document.body.parentNode.setAttribute('style', 'background-image: ' + pattern.dataUrl);
+}
+setTimeout(setBackground,1);
+setInterval(setBackground, 3000);
